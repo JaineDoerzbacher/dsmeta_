171d96d7e429fc0c325cbc61771ca98edd5a259b
jaine doerzbacher <doerzbacherjaine@gmail.com>
2022-07-13 22:49:58 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -16,14 +16,15 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 public class SaleService {
 
 	@Autowired
-	private SaleRepository  repository;
-	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
-		
+	private SaleRepository repository;
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
+
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		return repository.findSales(min, max, pageable);
-		
+
 	}
 }
